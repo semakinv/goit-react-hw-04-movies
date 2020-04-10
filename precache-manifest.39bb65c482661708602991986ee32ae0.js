b5abc8194a5af0f3e8373c41ab0eefc11efa04fb
Mykola.Somakin <k.semakin@gmail.com>
2020-04-10 18:14:46 +0300

    Updates

---
+++ precache-manifest.39bb65c482661708602991986ee32ae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d37887434e8441d9193458d6b39da52",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "4f559ed575519766293e",
+    "url": "/goit-react-hw-04-movies/static/css/main.ad66341b.chunk.css"
+  },
+  {
+    "revision": "b0283c99b4b4ae75af9e",
+    "url": "/goit-react-hw-04-movies/static/js/2.fc21a88a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-04-movies/static/js/2.fc21a88a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f559ed575519766293e",
+    "url": "/goit-react-hw-04-movies/static/js/main.0a770d2a.chunk.js"
+  },
+  {
+    "revision": "e34e4fd2a07b25362320",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.aa12acaf.js"
+  }
+]);
--- src/services/routes.js
+++
@@ -1,7 +0,0 @@
-export default {
-  homePage: '/',
-  moviesPage: '/movies',
-  movieDetailsPage: '/movies/:movieId',
-  cast: '/movies/:movieId/cast',
-  reviews: '/movies/:movieId/reviews',
-};
